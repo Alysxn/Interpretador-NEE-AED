6bfa1b9b61d14e7b90b68d65f8a159b897c58a5b
Alyson <alyson@gmail.com>
2024-05-24 11:52:49 -0400

    Inicio do projeto

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Alyson Souza Silva | Luiza Marinho Diniz Schirmer
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+// lista estática para armarzenar cada linha do código
+class ListaLinhas{
+
+};
+
+
+int main(){
+    // Definindo quantidade de linhas do interpretador
+
+    int quantLinhas = 100;
+
+
+
+
+    return 0;
+}
