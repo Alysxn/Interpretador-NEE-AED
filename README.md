7d9378e27c7aca902084aceac70a50c6e4a6ffea
Alyson <alysonsouza@gmail.com>
2024-06-04 00:41:04 -0300

    código finalizado

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 Projeto Prático 1 - Algoritmo e Estrutura de Dados
+
+
+Chat, fiz um interpretador de uma pseudo linguagem que funciona da seguinte forma
+
+a chamada de função é uma unica letra maiuscula seguida de espaço seguido de dois pontos:
+
+ex:
+
+A :
+
+após a  funcao temos duas possibilidades: 
+
+print identado com quatro espaços em branco com numero não negativo exemplo:
+
+    PRINT 1
+
+
+ou uma chamada para outra função, que é apenas uma letra maiuscula identado com 4 espaçoes em branco. exemplo: 
+
+
+    B
+após cada fim de bloco de código deve haver uma linha em branco para separar os blocos
+
+o fim do código sempre deve ser uma linha com apenas ~
+
+a função principal da linguagem é a Z :, que é de onde o programa iniciara, Z : pode estar em qualquer parte do código. segue um exemplo de codigo e sua saída:
+
+
+A :
+    PRINT 1
+    PRINT 2
+    B
+
+B :
+    PRINT 3
+    PRINT 5
+
+Z : 
+    A
+
+~
+
+
+a saída desse código seria : 1 2 3 5
+
+preciso que faça casos de testes complexos, porém que não ocorram erros de recursividade, por exemplo: função A que chama B que chama C que chama A
+
+isso ocorreria num looping infinito, por tanto, não deve ser usado como caso teste.
+
+Não faça nada além da sintaxe descrita, laços de repetições ou coisas do tipo
--- app.cpp
+++ app.cpp
@@ -5,40 +5,39 @@
 
 using namespace std;
 
+template<typename T>
 class NoFila{
-    public:
-    string print;
+public:
+    T print;
     NoFila *prox;
 
-    NoFila(): prox(nullptr) { }
+    NoFila() : prox(nullptr) {}
 
-    NoFila(string _print): print(_print), prox(nullptr) {}
+    NoFila(T _print) : print(_print), prox(nullptr) {}
 };
 
-
-
+template<typename T>
 class filaPrintNee{
-    private:
-    NoFila* frente;
-    NoFila* tras;
+private:
+    NoFila<T>* frente;
+    NoFila<T>* tras;
 
-    public:
+public:
     filaPrintNee(){
-        frente = new NoFila();
+        frente = new NoFila<T>();
         frente->prox = nullptr;
         tras = frente;
-    };
+    }
 
-    void enfilera(string numero){
-        tras->prox = new NoFila;
+    void enfilera(T numero){
+        tras->prox = new NoFila<T>();
         tras = tras->prox;
         tras->print = numero;
         tras->prox = nullptr;
-
     }
 
     void imprimeFila(){
-        NoFila* temp = frente->prox;
+        NoFila<T>* temp = frente->prox;
         cout << "Fila de numeros que serao printados: " << "\n";
         while (temp != nullptr) {
             std::cout << temp->print << " ";
@@ -46,58 +45,53 @@ class filaPrintNee{
         }
         std::cout << std::endl;
     }
-
 };
 
-
+template<typename T>
 class No{
-    public:
-    string funcao;
+public:
+    T funcao;
     bool vistada;
     No *prox;
 
-    No(): prox(nullptr) { }
+    No() : prox(nullptr) {}
 
-	No(string _funcao): funcao(_funcao), prox(nullptr) { }
-
+    No(T _funcao) : funcao(_funcao), prox(nullptr) {}
 };
 
-
-
+template<typename T>
 class PilhaFuncaoNee{
-    private:
-    No* topo;
-    No* fundo;
+private:
+    No<T>* topo;
+    No<T>* fundo;
 
-    public:
+public:
     PilhaFuncaoNee(){
-        fundo = new No();
+        fundo = new No<T>();
         fundo->prox = nullptr;
         topo = fundo;
-    };
+    }
 
-    void empilhaFuncao(string funcao){
-        No* aux = new No();
+    void empilhaFuncao(T funcao){
+        No<T>* aux = new No<T>();
         topo->funcao = funcao;
         topo->vistada = false;
         aux->prox = topo;
         topo = aux;
-    };
-
-    void imprimePilha(){
-        No* Notemp = topo;
-        cout << "funcoes Nee: \n";
-        while (Notemp != nullptr)
-        {
-            cout << Notemp->funcao << Notemp->vistada << "\n";
-            Notemp = Notemp->prox;
-            
-        }
-        
     }
 
-    bool buscaFuncao(string funcao) {
-        No* atual = topo;
+    void imprimePilha(){
+        No<T>* Notemp = topo;
+        cout << "Pilha de funcoes Nee: \n";
+        while (Notemp != nullptr)
+        {
+            cout << Notemp->funcao << "\n";
+            Notemp = Notemp->prox;
+        }
+    }
+
+    bool buscaFuncao(T funcao) {
+        No<T>* atual = topo;
         while (atual != nullptr) {
             if (atual->funcao == funcao) {
                 atual->vistada = true;
@@ -110,24 +104,34 @@ class PilhaFuncaoNee{
         return false;  // Função não encontrada
     }
 
-};
-
-
-class CodigoNee{
-    public:
-    string linha;
-    PilhaFuncaoNee *funcaoNee;
-
-
-    CodigoNee() : linha("") {
-        funcaoNee = new PilhaFuncaoNee();
+        void desalocaFuncoes() {
+        while (topo != fundo) {
+            No<T>* temp = topo;
+            topo = topo->prox;
+            delete temp;
+        }
+        delete fundo;  // Delete the sentinel node
+        fundo = nullptr;
+        topo = nullptr;
     }
 
-    CodigoNee(string _linha): linha(_linha){
-          funcaoNee = new PilhaFuncaoNee();
-    };
-    
-    void recebeLinha(string _linha){
+};
+
+template<typename T>
+class CodigoNee{
+public:
+    T linha;
+    PilhaFuncaoNee<T> *funcaoNee;
+
+    CodigoNee() : linha("") {
+        funcaoNee = new PilhaFuncaoNee<T>();
+    }
+
+    CodigoNee(T _linha) : linha(_linha) {
+        funcaoNee = new PilhaFuncaoNee<T>();
+    }
+
+    void recebeLinha(T _linha){
         linha = _linha;
         if(linha == "Z :"){
             funcaoNee->empilhaFuncao(linha);
@@ -137,47 +141,43 @@ class CodigoNee{
             funcaoNee->empilhaFuncao(linha);
         }
     }
-    
-    void verificaPrintNee(string p, filaPrintNee &fila){
+
+    void verificaPrintNee(T p, filaPrintNee<T> &fila){
         if(p.substr(0, 9) == "    PRINT"){
-            string printNumero = p.substr(10);
+            T printNumero = p.substr(10);
             if (printNumero.find_first_not_of("0123456789") == string::npos){
                 fila.enfilera(printNumero);
             }
         }
     }
 
-    bool verificaFuncao(string &funcao){
+    bool verificaFuncao(T &funcao){
         return funcao.length() == 5;
     }
 
-    string converteFuncao(string &funcao){
-        funcao.erase(0,4);
+    T converteFuncao(T &funcao){
+        funcao.erase(0, 4);
         return funcao + " :";
     }
-
 };
 
-
-
-
+template<typename T>
 class ListaCodigoNee{
-    public:
+public:
     int TAM;
     int n;
-    CodigoNee *codigo;
-    filaPrintNee *printNee;
-    PilhaFuncaoNee *pilhaNee;
+    CodigoNee<T> *codigo;
+    filaPrintNee<T> *printNee;
+    PilhaFuncaoNee<T> *pilhaNee;
 
-    
-    ListaCodigoNee(int _TAM): TAM(_TAM){
-        codigo = new CodigoNee[TAM + 1];
+    ListaCodigoNee(int _TAM) : TAM(_TAM){
+        codigo = new CodigoNee<T>[TAM + 1];
         n = 0;
-        printNee = new filaPrintNee();
-        pilhaNee = new PilhaFuncaoNee();
-    };
+        printNee = new filaPrintNee<T>();
+        pilhaNee = new PilhaFuncaoNee<T>();
+    }
 
-    void insereLinha(CodigoNee c){
+    void insereLinha(CodigoNee<T> c){
         if(n < TAM){
             n++;
             codigo[n] = c;
@@ -185,20 +185,20 @@ class ListaCodigoNee{
         else{
             cout << "Quantidade de linhas excedeu a capacidade do compilador.";
         }
-    };
+    }
 
-    int buscaFuncao(string funcao){
+    int buscaFuncao(T funcao){
         for(int i = 0; i < TAM; i++){
             if (codigo[i].linha == funcao)
             {
                 return i;
                 break;
-            } 
+            }
         }
         return 0;
-    };
+    }
 
-    void compilaFuncoes(string funcao){
+    void compilaFuncoes(T funcao){
         bool fimFuncao = false;
         int index = buscaFuncao(funcao);
         while (fimFuncao == false)
@@ -207,26 +207,23 @@ class ListaCodigoNee{
             codigo->verificaPrintNee(codigo[index].linha, *printNee);
             if(codigo->verificaFuncao(codigo[index].linha)){
                 // se for uma chamada de funcao, converte para FUNCAO + :
-                string funcao = codigo->converteFuncao(codigo[index].linha);
+                T funcao = codigo->converteFuncao(codigo[index].linha);
                 // passa a nova função para compilaFuncoes 
                 compilaFuncoes(funcao);
-
             }
 
             if (codigo[index].linha == "")
             {
                 fimFuncao = true;
             }
-            
         }
     }
-
 
     void compila(){
         int posicaoInicioZ = -1;
         int posicaoFinalZ = -1;
         int indexZ;
-        
+
         // busca Z :
         for(int i = 0; i < TAM; ++i){
             if(codigo[i].linha == "Z :"){
@@ -262,14 +259,13 @@ class ListaCodigoNee{
         while(indexZ != posicaoFinalZ)
         {
             // caso seja um print, adicionará a fila
-
             codigo->verificaPrintNee(codigo[indexZ].linha, *printNee);
 
             // verifica se é uma chamada de funcao
             if(codigo->verificaFuncao(codigo[indexZ].linha)){
                 // se for uma chamada de funcao, converte para FUNCAO + :
-                string funcao = codigo->converteFuncao(codigo[indexZ].linha);
-                
+                T funcao = codigo->converteFuncao(codigo[indexZ].linha);
+
                 compilaFuncoes(funcao);
             }
 
@@ -278,32 +274,38 @@ class ListaCodigoNee{
     }
 };
 
-
-
 int main(){
-
     int quantLinhas;
-    quantLinhas = 10000;
-    CodigoNee codigo("");
-    ListaCodigoNee l(quantLinhas);
+    quantLinhas = 300;
+    CodigoNee<string> codigo("");
+    ListaCodigoNee<string> l(quantLinhas);
     string linha;
 
     while(linha != "~"){
         getline(cin, linha);
         codigo.recebeLinha(linha);
         l.insereLinha(codigo);
-    };
-
+    }
 
     l.compila();
 
     cout << "\n";
+
     l.printNee->imprimeFila();
 
-    l.pilhaNee->imprimePilha();
+    cout << "\n";
 
+    codigo.funcaoNee->imprimePilha();
 
+    cout << "\n";
+
+    cout << "Fim da compilacao, removendo pilha de funcoes." << "\n";
+
+    codigo.funcaoNee->desalocaFuncoes();
+
+    cout << "\n";
     
-    return 0;
+    codigo.funcaoNee->imprimePilha();
 
+    return 0;
 }
